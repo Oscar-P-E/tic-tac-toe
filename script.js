f967e328979b9423a9ef2ff26a4c05087618dd0b
Oscar Elmahdy <opelm1@gmail.com>
2023-05-31 16:54:02 +1000

    Minor work

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// hello
+const ROWS = 3;
+const COLS = 3;
+
+const board = [[]];
+
+for (let i = 0; i < ROWS; ++i) {
+  for (let j = 0; j < COLS; ++j) {
+    board[j] = 1;
+  }
+    board[i] = 2;
+}
+
+console.log(board);
