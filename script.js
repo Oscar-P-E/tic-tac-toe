099112efe0035b2bfb85cbc44118c138a2ddd2ea
Oscar El <opelm1@gmail.com>
2023-06-08 21:00:10 +1000

    Make addBoard function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const ROWS = 3;
 const COLS = 3;
 
-const board = [];
-
-for (let i = 0, n = 1; i < ROWS; i++) {
-    board[i] = [];
-    for (let j = 0; j < COLS; j++) {
-        board[i][j] = n++;
+const drawBoard = (rows, cols) => {
+    const board = [];
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < cols; j++) {
+            board[i][j] = '';
+        }
     }
+    return board;
 }
+
+console.table(drawBoard(ROWS, COLS));
