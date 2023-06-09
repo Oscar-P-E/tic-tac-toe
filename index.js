5daef32d1d7d2fec962aca0e207e35d56a134ca9
Oscar El <opelm1@gmail.com>
2023-06-09 20:41:14 +1000

    Add main to index.html, minor update to index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 import { Gameboard, Player } from './game.js'
 
+const main = document.getElementById('main');
+const table = document.createElement('table');
+
+const createTable = () => {
+
+    const row = document.createElement('tr');
+
+    const cell = document.createElement('td');
+};
