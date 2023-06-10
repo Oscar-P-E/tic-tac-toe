fa6681576fabe4bfd75202d793cb60980f007f39
Oscar El <opelm1@gmail.com>
2023-06-10 18:34:28 +1000

    Write drawing of gameboard into a table functionality

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-import { Gameboard, Player } from './game.js'
+import { Gameboard, Player } from './game.js';
 
 const main = document.getElementById('main');
-const table = document.createElement('table');
 
 const createTable = () => {
+    const table = document.createElement('table');
+    table.classList.add('game-table');
 
-    const row = document.createElement('tr');
+    Gameboard.makeNewBoard(3, 3);
 
-    const cell = document.createElement('td');
+    const board = Gameboard.getBoard();
+    const rows = board.length;
+    const cols = board[0].length;
+
+    for (let i = 0; i < rows; i++) {
+        const tableRow = document.createElement('tr');
+        tableRow.classList.add('game-row');
+        table.appendChild(tableRow);
+        for (let j = 0; j < cols; j++) {
+            const tableCell = document.createElement('td');
+            tableCell.classList.add('game-cell');
+            tableCell.textContent = board[i][j];
+            tableRow.appendChild(tableCell);
+        };
+    };
+
+    main.appendChild(table);
 };
+
+createTable();
